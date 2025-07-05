0af941a1265aad15c5cd466a4bcccd0ec8884cb2
eddy <eddyboughioul@hotmail.fr>
2025-07-05 18:20:37 +0200

    generate proof for location

--- mopro-ui/react-native/app/(tabs)/location.tsx
+++ mopro-ui/react-native/app/(tabs)/location.tsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet, Button } from 'react-native';
+import { Platform, Text, View, StyleSheet, Button, ScrollView, TextInput } from 'react-native';
 import {
     generateNoirProof,
     verifyNoirProof
 } from "@/modules/mopro";
 import * as FileSystem from "expo-file-system";
 import * as Location from 'expo-location';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function App() {
     const [location, setLocation] = useState<Location.LocationObject | null>(null);
@@ -16,10 +18,13 @@ export default function App() {
     const [proof, setProof] = useState<Uint8Array>(new Uint8Array());
     const [isValid, setIsValid] = useState<string>("");
 
+    const [latitude, setLatitude] = useState(0);
+    const [longitude, setLongitude] = useState(0);
+
     async function genProof(): Promise<void> {
-        const a = Math.trunc(location?.coords.latitude || 0 * 1_000_000);
-        const b = Math.trunc(location?.coords.longitude || 0 * 1_000_000);
-        const circuitInputs = [a, b];
+        const lat = (latitude * 1_000_000).toFixed(0);
+        const long = (longitude * 1_000_000).toFixed(0);
+        const circuitInputs = [lat, long];
         if (Platform.OS === "web") {
             console.log("not implemented");
         } else if (Platform.OS === "android" || Platform.OS === "ios") {
@@ -51,6 +56,7 @@ export default function App() {
                 setProof(res);
             } catch (error) {
                 console.error("Error generating proof:", error);
+                setErrorMsg("Error generating proof: " + error);
             }
         }
     }
@@ -59,7 +65,7 @@ export default function App() {
         if (Platform.OS === "web") {
             setIsValid("not implemented");
         } else if (Platform.OS === "android" || Platform.OS === "ios") {
-            const circuitName = "noir_multiplier2.json";
+            const circuitName = "noir.json";
 
             const content = require(`@/assets/keys/${circuitName}`);
 
@@ -86,25 +92,33 @@ export default function App() {
                 setIsValid(res.toString());
             } catch (error) {
                 console.error("Error verifying proof:", error);
+                setErrorMsg("Error verifying proof: " + error);
             }
         }
     }
 
     useEffect(() => {
-        async function getCurrentLocation() {
 
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== 'granted') {
-                setErrorMsg('Permission to access location was denied');
-                return;
-            }
-
-            let location = await Location.getCurrentPositionAsync({});
-            setLocation(location);
-        }
 
         getCurrentLocation();
     }, []);
+
+    async function getCurrentLocation() {
+
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+            setErrorMsg('Permission to access location was denied');
+            return;
+        }
+
+        let location = await Location.getCurrentPositionAsync({});
+        setLocation(location);
+
+        if (location.coords) {
+            setLatitude(location.coords.latitude)
+            setLongitude(location.coords.longitude)
+        }
+    }
 
     let text = 'Waiting...';
     if (errorMsg) {
@@ -115,9 +129,41 @@ export default function App() {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.paragraph}>{text}</Text>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>Latitude</Text>
+                <TextInput
+                    style={styles.input}
+                    value={latitude.toString()}
+                    onChangeText={(e) => setLatitude(parseFloat(e))}
+                    keyboardType="decimal-pad"
+                />
+            </View>
+            <View style={styles.inputContainer}>
+                <Text style={styles.label}>Longitude</Text>
+                <TextInput
+                    style={styles.input}
+                    value={longitude.toString()}
+                    onChangeText={(e) => setLongitude(parseFloat(e))}
+                    keyboardType="decimal-pad"
+                />
+            </View>
+            <Text style={styles.errorText}>{errorMsg}</Text>
+            <Button title="Get location" onPress={() => getCurrentLocation()} />
             <Button title="Generate Noir Proof" onPress={() => genProof()} />
             <Button title="Verify Noir Proof" onPress={() => verifyProof()} />
+            <ThemedView style={styles.stepContainer}>
+                <ThemedText type="subtitle">Proof is Valid:</ThemedText>
+                <Text style={styles.output}>{isValid}</Text>
+                {/* TODO: add public signals */}
+                {/* <ThemedText type="subtitle">Public Signals:</ThemedText>
+                            <ScrollView style={styles.outputScroll}>
+                                <Text style={styles.output}>{JSON.stringify(inputs)}</Text>
+                            </ScrollView> */}
+                <ThemedText type="subtitle">Proof:</ThemedText>
+                <ScrollView style={styles.outputScroll}>
+                    <Text style={styles.output}>{proof}</Text>
+                </ScrollView>
+            </ThemedView>
         </View>
     );
 }
@@ -132,5 +178,40 @@ const styles = StyleSheet.create({
     paragraph: {
         fontSize: 18,
         textAlign: 'center',
+    },
+    errorText: {
+        fontSize: 18,
+        textAlign: 'center',
+        color: 'red'
+    },
+    outputScroll: {
+        maxHeight: 150,
+        borderWidth: 1,
+        borderColor: "gray",
+        marginBottom: 10,
+    },
+    output: {
+        fontSize: 14,
+        padding: 10,
+    },
+    stepContainer: {
+        gap: 8,
+        marginBottom: 8,
+    },
+    input: {
+        height: 40,
+        borderColor: "gray",
+        borderWidth: 1,
+        flex: 1,
+        paddingHorizontal: 10,
+    },
+    inputContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    label: {
+        fontSize: 16,
+        marginRight: 10,
     },
 });
